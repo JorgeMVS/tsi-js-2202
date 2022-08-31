5eb0042ec755b62ccabaa2ffcc8c1d57605e0370
Jorge Miguel <jorgemvs578@gmail.com>
2022-08-31 20:46:11 -0300

    exercicio imc

---
+++ exercjs/calculadora.js
@@ -0,0 +1,7 @@
+let altura = parseFloat(prompt("Qual sua altura?"));
+
+let peso = parseFloat(prompt("Qual seu peso?"));
+
+let res = parseInt(peso /(altura * altura));
+
+alert("Seu IMC é: " + res);
