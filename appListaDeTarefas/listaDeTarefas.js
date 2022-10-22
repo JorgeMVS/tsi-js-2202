f77aab3bb563db711457b99fa31016bd9e6287a4
Jorge Miguel <jorgemvs578@gmail.com>
2022-10-21 22:15:27 -0300

    filtar e deletar tudo em lista de tarefas

--- appListaDeTarefas/listaDeTarefas.js
+++ appListaDeTarefas/listaDeTarefas.js
@@ -8,8 +8,54 @@ function carregaMonitoresDeEventos(){
     
     btnAddTarefa.addEventListener('click',addTarefa)
     listaTarefa.addEventListener('click',apagarTarefa)
+    btnLimpaTudo.addEventListener('click',limparTarefas)
+    filtroTarefa.addEventListener('keyup',filtrarTarefas)
 }
 carregaMonitoresDeEventos();
+
+
+
+function filtrarTarefas(evento)
+{
+    //toLowerCase deixa tudo minusculo
+    const procurado = evento.target.value.toLowerCase();
+
+    const tarefas = document.querySelectorAll('.collection-item')
+
+   //para cada tarefa existente busque a string desejada
+   tarefas.forEach(function(tarefa){
+
+    //recuperamos apenas o texto do elemento <li> onde esta a tarefa
+    textoTarefa = tarefa.innerText;
+
+    //procuramos a string digita no filtro no texto que está o li
+
+    //se false (diferente de -1) ele retorna -1 (não false)
+    if (textoTarefa.toLowerCase().indexOf(procurado) != -1) 
+    {
+        tarefa.style.display='block';
+    } 
+    
+    else
+    {
+        tarefa.style.display='none';
+    }
+
+
+   });
+}
+
+
+
+function limparTarefas(evento)
+
+{
+    evento.preventDefault();
+    listaTarefa.innerHTML ='';
+
+}
+
+
 
 function apagarTarefa(evento)
     {
@@ -29,21 +75,47 @@ function addTarefa(evento){// chama o evento q acontece acima com o
     if(entradaTarefa.value===''){
         alert('entre com uma  tarefa');
     }
+    else
+    {
+        const li =document.createElement('li');
+        li.className='collection-item';
+        li.appendChild(document.createTextNode(entradaTarefa.value));
     
-    const li =document.createElement('li');
-    li.className='collection-item';
-    li.appendChild(document.createTextNode(entradaTarefa.value));
+        const a = document.createElement('a');
+        a.className='apaga-tarefa secondary-content';
+    
+        const i=document.createElement('i'); //chama no html a fonte
+        i.className='fa fa-remove';// vem do fontawesome,icone do x
+                                   
+    
+        //monta o elemneto em li para colocar em ul 
+        a.appendChild(i);
+        li.appendChild(a);
+        listaTarefa.appendChild(li);
 
-    const a = document.createElement('a');
-    a.className='apaga-tarefa secondary-content';
+        //gravar tarefa no localStorage
+        gravarTarefa(entradaTarefa.value);
+    }
+}
+    
+   function gravarTarefa(tarefa){
+    
+    let tarefas = [];
+    //verifica se já há algo gravado no banco
+    let tarefasDoStorage = localStorage.getItem('tarefas');
 
-    const i=document.createElement('i'); //chama no html a fonte
-    i.className='fa fa-remove';// vem do fontawesome,icone do x
-                               
+    if (tarefasDoStorage != null) {
+        
+         //com o metodo JSON.stringify() tranformamos vetores e objs em estrings
+        tarefas = JSON.parse(tarefasDoStorage);
+    }
+       
 
-    //monta o elemneto em li para colocar em ul 
-    a.appendChild(i);
-    li.appendChild(a);
-    listaTarefa.appendChild(li);
+    tarefas.push(tarefa);
 
-}   
+    localStorage.setItem('tarefas',JSON.stringify(tarefas));
+}
+
+
+
+
